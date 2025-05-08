f3e7864b48a8c76619bb9f1e91c2fa0ded801b6d
Long Pham <philong.bmt@gmail.com>
2025-05-08 10:05:36 +0700

    init app

---
+++ ios/Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Long Pham on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
