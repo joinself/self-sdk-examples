3592be17960e67d25e7ac432e7a5bf0b601e3271
Long Pham <philong.bmt@gmail.com>
2025-05-08 14:32:15 +0700

    add lfc flow

---
+++ ios/Example/Example/OnboardingViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  OnboardingViewModel.swift
+//  Example
+//
+//  Created by Long Pham on 8/5/25.
+//
+
+import Foundation
+import Combine
+import self_ios_sdk
+
+final class OnboardingViewModel: ObservableObject {
+    @Published var isOnboardingCompleted: Bool = false
+    
+    let account = Account.Builder()
+        .withEnvironment(Environment.preview)
+        .withSandbox(true) // if true -> production
+        .withGroupId("") // ex: com.example.app.your_app_group
+        .withStoragePath(FileManager.storagePath)
+        .build()
+    
+    func lfcFlow() {
+        SelfSDK.showLiveness(account: account, showIntroduction: true, autoDismiss: true, onResult: { selfieImageData, credentials, error in
+            print("showLivenessCheck credentials: \(credentials)")
+        })
+    }
+}
+
+extension FileManager {
+    static var storagePath: String {
+        let storagePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("account1")
+        createFolderAtURL(url: storagePath) // create folder if needed
+        print("StoragePath: \(storagePath)")
+        return storagePath.path()
+    }
+    
+    static func createFolderAtURL(url: URL) {
+        if FileManager.default.fileExists(atPath: url.path()) {
+            print("Folder already exists: \(url)")
+            
+        } else {
+            // Create the folder
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+                print("Folder created successfully: \(url)")
+            } catch {
+                print("Error creating folder: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
