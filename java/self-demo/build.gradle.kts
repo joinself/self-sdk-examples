a686f272271aa81551d6fe14e2cb76ed76b46522
Vu Do <vu@joinself.com>
2025-06-17 10:59:44 +0700

    add self-demo jvm server

---
+++ java/self-demo/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.jvm)
+    id("application")
+}
+
+dependencies {
+    implementation("com.joinself:sdk-jvm:1.0.0")
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.coroutines)
+    implementation(libs.kotlin.serialization)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17)) // Set to the desired Java version
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass = "com.joinself.MainKt"
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
