0fd943095aeef751072829e361e592302a80cf29
Vu Do <vu@joinself.com>
2025-06-24 14:44:06 +0700

    add comments

--- android/SelfDemo/app/src/main/java/com/joinself/app/demo/ui/screens/DocSignResultScreen.kt
+++ android/SelfDemo/app/src/main/java/com/joinself/app/demo/ui/screens/DocSignResultScreen.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.joinself.app.demo.ui.ServerRequestState
+import com.joinself.app.demo.ServerRequestState
 
 @Composable
 fun DocSignResultScreen(
