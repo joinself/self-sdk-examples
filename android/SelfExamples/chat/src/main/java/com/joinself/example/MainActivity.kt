b595678966fc39a41ac6e4a57af7d05552e386a4
Mark Newman <mark@joinself.com>
2025-07-15 17:36:07 +0100

    feat: add android examples

---
+++ android/SelfExamples/chat/src/main/java/com/joinself/example/MainActivity.kt
@@ -0,0 +1,262 @@
+package com.joinself.example
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.joinself.common.Environment
+import com.joinself.common.exception.InvalidCredentialException
+import com.joinself.sdk.SelfSDK
+import com.joinself.sdk.models.Account
+import com.joinself.sdk.models.ChatMessage
+import com.joinself.sdk.models.Receipt
+import com.joinself.sdk.ui.addLivenessCheckRoute
+import com.joinself.ui.theme.SelfModifier
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // init the sdk
+        SelfSDK.initialize(applicationContext,
+            pushToken = null,
+            log = { Log.d("Self", it) }
+        )
+
+        // the sdk will store data in this directory, make sure it exists.
+        val storagePath = File(applicationContext.filesDir.absolutePath + "/account1")
+        if (!storagePath.exists()) storagePath.mkdirs()
+
+        val account = Account.Builder()
+            .setContext(applicationContext)
+            .setEnvironment(Environment.production)
+            .setSandbox(true)
+            .setStoragePath(storagePath.absolutePath)
+            .build()
+
+        setContent {
+            val coroutineScope = rememberCoroutineScope()
+            val navController = rememberNavController()
+            val selfModifier = SelfModifier.sdk()
+
+            var isRegistered by remember { mutableStateOf(account.registered()) }
+            var groupAddress by remember { mutableStateOf("") }
+            var serverInboxAddress by remember { mutableStateOf("") }
+
+            var inputMessage by remember { mutableStateOf("") }
+            val messages = remember { mutableStateListOf<String>() }
+
+            var statusText by remember { mutableStateOf("") }
+
+            // connect with server by an inbox address, a group address is returned. The group address is used to send chat message.
+            fun connect() {
+                statusText = ""
+                coroutineScope.launch(Dispatchers.IO) {
+                    try {
+                        val gAdress = account.connectWith(serverInboxAddress, info = mapOf())
+                        if (gAdress.isNotEmpty()) {
+                            groupAddress = gAdress
+
+                            statusText = "group address: $gAdress"
+                        }
+                    } catch (ex: Exception) {
+                        Log.e("Self", ex.message, ex)
+                        statusText = "wrong server address"
+                    }
+                }
+            }
+
+            fun sendChat() {
+                // build a chat message
+                val chat = ChatMessage.Builder()
+                    .setToIdentifier(groupAddress)
+                    .setMessage(inputMessage)
+                    .build()
+
+                // send chat to server
+                coroutineScope.launch(Dispatchers.IO) {
+                    account.send(chat) { messageId, _ ->
+                        messages.add(inputMessage)
+                        inputMessage = ""
+                    }
+                }
+            }
+
+            // send delivered receipt
+            fun sendReceipt(message: ChatMessage) {
+                val receipt = Receipt.Builder()
+                    .setToIdentifier(groupAddress)
+                    .setDelivered(listOf(message.id()))
+                    .build()
+                coroutineScope.launch(Dispatchers.IO) {
+                    account.send(receipt)
+                }
+            }
+
+            LaunchedEffect(Unit) {
+                // need to wait for account connected
+                account.setOnStatusListener { status ->
+                    println("onStatus $status")
+                }
+
+                // listen to messages from server
+                account.setOnMessageListener { msg ->
+                    when (msg) {
+                        is ChatMessage -> {
+                            messages.add(msg.message()) // append text to the message list
+
+                            sendReceipt(msg) // send delivered receipt
+                        }
+                        is Receipt -> {
+                            println("receipt message")
+                        }
+                    }
+                }
+            }
+
+            NavHost(navController = navController,
+                startDestination = "main",
+                modifier = Modifier.systemBarsPadding(),
+                enterTransition = { EnterTransition.None },
+                exitTransition = { ExitTransition.None }
+            ) {
+                composable("main") {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .padding(start = 8.dp, end = 8.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(modifier = Modifier.padding(top = 40.dp), text = "Registered: ${isRegistered}")
+                        Button(
+                            onClick = {
+                                navController.navigate("livenessRoute")
+                            },
+                            enabled = !isRegistered
+                        ) {
+                            Text(text = "Create Account")
+                        }
+
+                        // connect to server
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(4.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            TextField(modifier = Modifier.weight(1f), enabled = isRegistered && groupAddress.isEmpty(),
+                                value = serverInboxAddress,
+                                onValueChange = { serverInboxAddress = it },
+                                placeholder = { Text("enter server inbox address") }
+                            )
+                            Button(
+                                modifier = Modifier.width(80.dp), contentPadding = PaddingValues(0.dp),
+                                onClick = {
+                                    connect()
+                                },
+                                enabled = isRegistered && groupAddress.isEmpty() && serverInboxAddress.isNotEmpty(),
+                            ) {
+                                Text(text = "Connect")
+                            }
+                        }
+                        Text(text = statusText)
+
+                        // chat input
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(4.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            TextField(modifier = Modifier.weight(1f),
+                                value = inputMessage,
+                                onValueChange = { inputMessage = it },
+                                enabled = groupAddress.isNotEmpty(),
+                                placeholder = { Text("enter chat message") }
+                            )
+                            Button(
+                                modifier = Modifier.width(80.dp), contentPadding = PaddingValues(0.dp),
+                                onClick = { sendChat() },
+                                enabled = isRegistered && groupAddress.isNotEmpty(),
+                            ) {
+                                Text(text = "Send")
+                            }
+                        }
+                        LazyColumn(modifier = Modifier.fillMaxSize().background(Color.LightGray)) {
+                            items(messages) { msg ->
+                                Text(
+                                    text = msg,
+                                    modifier = Modifier.padding(start = 4.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+
+                // add liveness check to main navigation
+                addLivenessCheckRoute(navController, route = "livenessRoute", selfModifier = selfModifier,
+                    account = { account },
+                    withCredential = true,
+                    onFinish = { selfie, credentials ->
+                        if (!account.registered()) {
+                            coroutineScope.launch(Dispatchers.IO) {
+                                try {
+                                    if (selfie.isNotEmpty() && credentials.isNotEmpty()) {
+                                        val success = account.register(selfieImage = selfie, credentials = credentials)
+                                        if (success) {
+                                            isRegistered = true
+                                            withContext(Dispatchers.Main) {
+                                                Toast.makeText(applicationContext, "Register account successfully", Toast.LENGTH_LONG).show()
+                                            }
+                                        }
+                                    }
+                                } catch (_: InvalidCredentialException) { }
+                            }
+                        }
+
+                        // nav back to main
+                        coroutineScope.launch(Dispatchers.Main) {
+                            navController.popBackStack("livenessRoute", true)
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
